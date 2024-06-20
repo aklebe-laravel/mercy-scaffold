148098e9e72c8e00b5cf3ebf6447e5829ab8fce7
Andreas Klebe <a.klebe.work@gmail.com>
2024-06-20 17:01:37 +0200

    fixed shell db check

--- bash-scripts/functions/mysql.sh
+++ bash-scripts/functions/mysql.sh
@@ -10,8 +10,9 @@
 #   error code or 0
 # ==========================================================================
 function f_mysql_has_database() {
-#  echo -ne "Checking database '$destination_db_name' exists ... "
-  mysqlshow --host=$destination_db_host --port=$destination_db_port --user=$destination_db_user --password=$destination_db_password | grep -i -E -w "$destination_db_name" 1> /dev/null
-#  echo -e "ready.\r"
-  return $?
+#  Find safe method to check database exists, its not working on all servers/users
+   return 0
+
+#  mysqlshow --host=$destination_db_host --port=$destination_db_port --user=$destination_db_user --password=$destination_db_password | grep -i -E -w "$destination_db_name" 1> /dev/null
+#  return $?
 }
